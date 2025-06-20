e9b359faefaaf8fe69e2e4b424d7fc8b7e2125cc
Mykhailo Konstantinov <mkonst10@gmail.com>
2025-06-20 14:48:19 +0300

    update crypto context

--- src/context/crypto-context.jsx
+++ src/context/crypto-context.jsx
@@ -13,6 +13,20 @@ export function CryptoContextProvider({ children }) {
   const [crypto, setCrypto] = useState([]);
   const [assets, setAssets] = useState([]);
 
+  function mapAssets(assets, result) {
+    return assets.map((asset) => {
+      const coin = result.find((c) => c.id === asset.id);
+      return {
+        grow: asset.price < coin.price,
+        growPercent: percentDifference(asset.price, coin.price),
+        totalAmount: asset.amount * coin.price,
+        totalProfit: asset.amount * coin.price - asset.amount * asset.price,
+        name: coin.name,
+        ...asset,
+      };
+    });
+  }
+
   useEffect(() => {
     async function preload() {
       setLoading(true);
@@ -20,26 +34,17 @@ export function CryptoContextProvider({ children }) {
       const assets = await fetchAssets();
 
       setCrypto(result);
-      setAssets(
-        assets.map((asset) => {
-          const coin = result.find((c) => c.id === asset.id);
-          return {
-            grow: asset.price < coin.price,
-            growPercent: percentDifference(asset.price, coin.price),
-            totalAmount: asset.amount * coin.price,
-            totalProfit: asset.amount * coin.price - asset.amount * asset.price,
-            name: coin.name,
-            ...asset,
-          };
-        })
-      );
+      setAssets(mapAssets(assets, result));
       setLoading(false);
     }
     preload();
   }, []);
 
+  function addAsset(newAsset) {
+    setAssets((prev) => mapAssets([...prev, newAsset], crypto));
+  }
   return (
-    <CryptoContext.Provider value={{ loading, crypto, assets }}>
+    <CryptoContext.Provider value={{ loading, crypto, assets, addAsset }}>
       {children}
     </CryptoContext.Provider>
   );
